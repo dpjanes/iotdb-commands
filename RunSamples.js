93f4985d45718e1cb2dc3fa19a22a4d03a353935
David Janes <davidjanes@davidjanes.com>
2015-12-31 10:44:33 -0330

    run and test samples (a work in progress)

--- RunSamples.js
+++ RunSamples.js
@@ -26,6 +26,8 @@
 var iotdb = require('iotdb');
 var _ = iotdb._;
 
+var FSTransport = require('iotdb-transport-fs').Transport;
+
 var fs = require('fs');
 var path = require('path');
 var util = require('util');
@@ -36,39 +38,102 @@ var ad = require('minimist')(process.argv.slice(2), {
 });
 
 // --- main ---
-var json_paths = [];
-if (ad.all) {
-    var samples_dir = "samples";
-
-    var names = fs.readdirSync(samples_dir);
-    names.map(function (name) {
-        if (!name.match(/[.]json$/)) {
-            return;
-        }
-
-        json_paths.push(path.join("samples", name));
+var load_transporter = function(contextd, done) {
+    contextd.transporter = new FSTransport({
+        prefix: "samples/things",
     });
-} else if (ad._.length) {
-    json_paths = ad._;
-}
 
-var run = function (json_path, done) {
-    console.log(json_path);
-    return done(null, null);
+    done(null, null);
 };
 
-var run_next = function () {
-    if (json_paths.length === 0) {
-        process.nextTick(process.exit);
-        return;
+/**
+ *  This will callback with all the metas, then null
+ */
+var metas = function(contextd, callback) {
+    // console.log("HERE:XXX", contextd.transporter);
+    var count = 0;
+    var _increment = function() {
+        count++;
+    };
+    var _decrement = function() {
+        if (--count === 0) {
+            callback(null, null);
+            callback = function() {};
+        }
     }
 
-    var json_path = json_paths[0];
-    json_paths.splice(0, 1);
-
-    run(json_path, function(error, result) {
-        run_next();
+    _increment();
+    contextd.transporter.list(function(ld) {
+        if (ld.id) {
+            _increment();
+            contextd.transporter.get({
+                id: ld.id,
+                band: "meta",
+            }, function(gd) {
+                if (gd.value) {
+                    callback(null, gd);
+                }
+                _decrement();
+            });
+        } else if (ld.end) {
+            _decrement();
+            // return callback(null, null);
+        }
     });
 };
 
-run_next();
+var run_one = function (contextd, done) {
+    var json = require("./" + contextd.json_path);
+    console.log("PATH", contextd.json_path);
+    metas(contextd, function(error, metad) {
+        if (metad === null) {
+            return done(null, null);
+        }
+        console.log("META", json, metad);
+    });
+};
+
+var run_next = function (contextd, done) {
+    if (contextd.json_paths.length === 0) {
+        return done(null, null);
+    }
+
+    contextd.json_path = contextd.json_paths[0];
+    contextd.json_paths.splice(0, 1);
+
+    run_one(contextd, function(error, result) {
+        run_next(contextd, done);
+    });
+};
+
+var main = function() {
+    var json_paths = [];
+    if (ad.all) {
+        var samples_dir = "samples";
+
+        var names = fs.readdirSync(samples_dir);
+        names.map(function (name) {
+            if (!name.match(/[.]json$/)) {
+                return;
+            }
+
+            json_paths.push(path.join("samples", name));
+        });
+    } else if (ad._.length) {
+        json_paths = ad._;
+    }
+
+    var contextd = {
+        json_paths: json_paths,
+        ad: ad,
+    };
+
+    load_transporter(contextd, function(error, result) {
+        run_next(contextd, function(error, result) {
+            process.nextTick(process.exit);
+        });
+    });
+
+};
+
+main();
