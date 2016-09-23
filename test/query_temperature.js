6f1cf10ddbfba981e7f5d572c6285fb29f8c4cf8
David Janes <davidjanes@davidjanes.com>
2016-09-23 08:09:24 -0400

    query testing

---
+++ test/query_temperature.js
@@ -0,0 +1,57 @@
+/*
+ *  query_temperature.js
+ *
+ *  David Janes
+ *  IOTDB.org
+ *  2016-09-23
+ *
+ *  Copyright [2013-2016] [David P. Janes]
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+"use strict";
+
+const assert = require("assert");
+const helpers = require("./helpers");
+
+const iotdb_thing = require("..");
+
+describe("query_temperature", function() {
+    const query = "temperature";
+
+    it("thing:all", function(done) {
+        const thing = null;
+
+        helpers.run({
+            query: query,
+            thing: thing,
+        }, (error, matches) => {
+            try {
+                assert.ok(!error, "no error expected");
+
+                assert.deepEqual(helpers.select(matches, 'thing-basement-heater').response, 'the temperature is 20');
+                assert.deepEqual(helpers.select(matches, 'thing-main-thermometer').response, 'the temperature is 70');
+                assert.deepEqual(helpers.select(matches, 'thing-master-ac').response, 'the temperature is 16');
+
+                done();
+            }
+            catch (x) {
+                done(x);
+            }
+        });
+    });
+});
+
+                    
+
